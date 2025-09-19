a47988c8f63030ef63985e9e9091a40f71810849
Mehmet Çobanoğlu <mehmetcobanoglu658@gmail.com>
2025-09-19 23:57:49 +0300

    .

--- Spring-3/spring-data-jpa/src/main/java/com/mehmetcobanoglu/controller/IStudentController.java
+++ Spring-3/spring-data-jpa/src/main/java/com/mehmetcobanoglu/controller/IStudentController.java
@@ -15,4 +15,6 @@ public interface IStudentController {
     Student getStudentById(Integer id);
 
     public void deleteStudent(Integer id);
+
+    public Student updateStudent(Integer id, Student updateStudent);
 }
--- Spring-3/spring-data-jpa/src/main/java/com/mehmetcobanoglu/services/IStudentService.java
+++ Spring-3/spring-data-jpa/src/main/java/com/mehmetcobanoglu/services/IStudentService.java
@@ -13,4 +13,7 @@ public interface IStudentService {
     public Student getStudentById(Integer id);
 
     public void deleteStudent(Integer id);
+
+    public Student updateStudent(Integer id, Student updatStudent);
+
 }
